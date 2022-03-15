4670cb13a0ec30b08575d3c75953de3b283a9003
Kevin Gallo <Gallok731@gmail.com>
2022-03-15 17:06:51 -0400

    commit tree 67ab1eef6444ef680fa2ec34ac7c3c3ac0c6e2df

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,32 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    # if params[:name]
+    #   name = params[:name].capitalize
+    #   puts name
+    #   if Student.where({first_name: name})
+    #     student = Student.where({first_name: name})
+    #   elsif Student.where({last_name: name})
+    #     student = Student.where({last_name: name})
+    #   end
+    # else
+    #   student = Student.all
+    # end
+    if (params[:name])
+      name = params[:name].capitalize()
+      if (Student.where({first_name: name}))
+        student = Student.where({first_name: name})
+      elsif (Student.where({last_name: name}))
+        student = Student.where({last_name: name})
+      end
+    else 
+      student = Student.all
+    end
+    render json: student
+  end
+
+  def show
+    students = Student.find(params[:id])
     render json: students
   end
 
